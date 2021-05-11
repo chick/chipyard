7c971cf982ffdae5eb4200a160a2ad2e741710b2
chick <chick@qrhino.com>
2021-05-11 10:21:06 -0700

    Try building out more steps with using

--- scripts/build-actions-ci.sh
+++ scripts/build-actions-ci.sh
@@ -1,4 +1,8 @@
 
-python -m actions_includes ./.github/actions/current-working.yml - | \
+# use python virtual env
+# source ~/.virtualenvs/python-for-github-actions/bin/activate
+
+MAIN_FILE=$1
+python -m actions_includes $MAIN_FILE - | \
   sed 's/continue-on-error: false/continue-on-error: true/' > .github/workflows/ci.yml
 cat .github/workflows/ci.yml
